da49933063b0e31ba1ce99998bd938bcc58df4f2
Danil Borchevkin <danil.borchevkin@gmail.com>
2024-10-05 23:37:54 +0300

    Add template

---
+++ inc/errors.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#define ERR_OK      (0)
+#define ERR_NG      (ERR_OK + 1)
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "errors.hpp"
+
+int main(int argc, char *argv[]) {
+    std::cout << "Muridae is started" << std::endl;
+    
+    std::cout << "Muridae is finished" << std::endl;
+    return 0;
+}
